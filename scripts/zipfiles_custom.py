e643d1e22b1ecb564bbf9a28296d3ee1257eb8fe
Luca De Grandis <100571769+LucaDeGrandis@users.noreply.github.com>
2023-10-09 14:49:59 +0200

    Update zipfiles_custom.py

--- scripts/zipfiles_custom.py
+++ scripts/zipfiles_custom.py
@@ -14,3 +14,18 @@ def zip_files(file_paths, zip_path):
     with zipfile.ZipFile(zip_path, 'w') as zip_file:
         for file_path in file_paths:
             zip_file.write(file_path)
+
+
+def unzip_file(file_path, destination_folder):
+    """
+    Extracts all files from a zip archive to a specified folder.
+
+    Args:
+        file_path (str): The path to the zip archive.
+        destination_folder (str): The path to the folder where the files will be extracted.
+
+    Returns:
+        None
+    """
+    with zipfile.ZipFile(file_path, 'r') as zip_ref:
+        zip_ref.extractall(destination_folder)
